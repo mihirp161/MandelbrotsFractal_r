ae4b6215b1757cda9cc505a3950f9e2b61ccd622
Mihir Patel <47681434+ToadHanks@users.noreply.github.com>
2020-01-18 00:10:07 -0500

    Add files via upload

--- totally_unoptimized_mandelbrot.R
+++ totally_unoptimized_mandelbrot.R
@@ -1,30 +1,32 @@
 options(warn=-1) #Suppress the warning coming from the color pacakge
 library(scico)
 
-lajolla <- scico::scico(250, palette= 'lajolla') #picked a color scheme
-color <- c(lajolla)
+tokyo <- scico::scico(250, palette= 'tokyo') #picked a color scheme
+color <- c(tokyo)
 
 # A fractal function
 mandelbrot <<- function(i,j){
   
-  bound <- 1 #flag/swtich
-  x <- i-460 # curves
-  y <- 240-j # cracks
-
-  a <- complex(real = x/200, imaginary = y/200) #real numbers
-  b <- complex(real = 0, imaginary = 0) #imaginary numbers
+  bound <- 1 #flag/swtich if number has escaped the M-Set
+  x <- i-460 
+  y <- 240-j 
+  
+  # A 2d complaix plain
+  a <- complex(real = x/200, imaginary = y/200) 
+  b <- complex(real = 0, imaginary = 0) 
 
   for(k in seq(length(color))){
-    # I am doing with a b^2+ba sequence to make fracatl rings
+    # Expression to make M-Set
     b <- b*b+a 
-    if(Mod(b)>2){
+    
+    if(Mod(b)>2){ #Region color for escaped numbers
       color <- k
       bound <- 0
       break;
     }
   }
   
-  return(list(flag= bound, c= color))
+  return(list(flag= bound, c= color)) #return the bound with associated color
 }
 
 png(filename="mandelbrot_bad.png", bg= "black",height = 600, width = 600, units = "px") 
@@ -32,16 +34,16 @@ png(filename="mandelbrot_bad.png", bg= "black",height = 600, width = 600, units 
 #plot attributes
 plot(c(50, 600), c(0,500), type = 'l', xaxt='n',yaxt='n', ann=FALSE)
 
-# iterations
-for(i in 1:1920){ 
-  for(j in 1:1080){
+# iterations for x and y
+for(i in 1:2020){ 
+  for(j in 1:2019){
     f_x= mandelbrot(i,j) 
     
-    if(f_x$flag){ #if false, do nothing
-      points(i,j,pch=46) #line graph without a color brew
+    if(f_x$flag){ 
+      points(i,j,pch=46) 
     }
     else{
-      points(i,j,pch=46,col=color[f_x$c]) #color line if flag is True
+      points(i,j,pch=46,col=color[f_x$c]) #color region if flag is True
     }
   }
 }
